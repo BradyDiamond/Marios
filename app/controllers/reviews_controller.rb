4aa26ff9056c4ce73a967463d3ecceeda59d67af
brady diamond <brady.diamond27@gmail.com>
2022-05-27 16:57:12 -0700

    install troubleshoot faker

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,6 +9,7 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
     if @review.save
+      flash[:notice] = "Review successfully posted!"
       redirect_to product_path(@product)
     else
       render :new
@@ -30,6 +31,7 @@ class ReviewsController < ApplicationController
   def update
     @review = Review.find(params[:id])
     if @review.update(review_params)
+      flash[:notice] = "Review successfully updated!"
       redirect_to product_path(@review.product)
     else
       @album = Album.find(params[:album_id])
@@ -41,10 +43,6 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
     @review.destroy
     redirect_to product_path(@review.product)
-  end
-
-  def most_reviews
-  product = Product.most_reviews
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+db/seeds.rb
+Product.destroy_all
+
+50.times do |index|
+  Product.create!(name: Faker::Food.spice,
+                        cost: Faker::Lorem.number(20, false, 0).chop,
+                        country_of_origin: "USA")
+                        250.times do |index|
+                          Product.create!(name: Faker::Food.spice,
+                        cost: Faker::Lorem.sentence(20, false, 0).chop,
+                        country_of_origin: "USA")
+end
+
+
+p "Created #{Spice.count} spices"
