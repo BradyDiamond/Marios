24c24c32077431a8139df0b9b752a79309b4b93d
brady diamond <brady.diamond27@gmail.com>
2022-06-03 09:48:52 -0700

    scope test 1

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,4 +9,10 @@ describe Product do
     product = Product.create({name: "product", cost: 9, country_of_origin: "USA"})
     expect(product.name()).to(eq("Product"))
   end
+  it("returns the last 3 added products") do
+    product1 = Product.create({name: "product1", cost: 9, country_of_origin: "USA"})
+    product2 = Product.create({name: "product2", cost: 9, country_of_origin: "USA"})
+    product3 = Product.create({name: "product3", cost: 9, country_of_origin: "USA"})
+    expect(Product.three_most_recent()).to(eq([product3, product2, product1]))
+  end
 end
