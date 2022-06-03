bfb2f5307a59fe2ba4c16370a152aa13d3b24f2a
brady diamond <brady.diamond27@gmail.com>
2022-06-03 09:25:34 -0700

    write reviiew test 5

--- spec/models/reviews_spec.rb
+++ spec/models/reviews_spec.rb
@@ -5,4 +5,10 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating}
+  it("titleizes the name of an author") do
+    product = Product.create({name: "product", cost: 9, country_of_origin: "USA"})
+    expect(product.name()).to(eq("Product"))
+    review = Review.create({product_id: product.id, author: "gary", content_body: "Reiciendis quo in. Delectus vero sapiente. Conseq.", rating: 1})
+    expect(review.author()).to(eq("Gary"))
+  end
 end
